a08dc7b4076a23644cdb778f4f8624af1d4d1d34
Matheus <matheus.possenti24@gmail.com>
2024-10-14 14:54:42 -0300

    configurando eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,4 @@
-import "dotenv/config"
+import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
@@ -8,7 +8,7 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false){
+if (_env.success === false) {
   console.error('Invalid environment variables:', _env.error.format())
 
   throw new Error('Invalid environment variables')
