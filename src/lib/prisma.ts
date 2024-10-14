b0a6e89eb74b17d9f5627ac5047088d4e1880564
Matheus <matheus.possenti24@gmail.com>
2024-10-14 18:44:10 -0300

    criando controller de registro

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
-export const primsa = new PrismaClient({
+export const prisma = new PrismaClient({
   log: env.NODE_ENV === 'dev' ? ['query'] : [],
 })
