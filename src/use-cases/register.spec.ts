e1bdf0ec0635767f2aef83453b64a1f5419c0624
Matheus <matheus.possenti24@gmail.com>
2024-10-15 17:59:31 -0300

    firts unit tests

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,33 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+    })
+
+    const isPasswordHashValid = await compare('password', user.password_hash)
+
+    expect(isPasswordHashValid).toBe(true)
+  })
+})
