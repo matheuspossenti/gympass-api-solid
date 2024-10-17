3f45f6b1e7ae388b59db15dde936c2cdde4fe434
Matheus <matheus.possenti24@gmail.com>
2024-10-17 15:57:22 -0300

    search gym by name use case and metrics use case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,4 +4,5 @@ export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   findByUserIdOnDate(userId: string, data: Date): Promise<CheckIn | null>
+  countByUserId(userId: string): Promise<number>
 }
---
+++ src/use-cases/get-user-metrics.ts
@@ -0,0 +1,21 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private checkInsRespository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkInsCount = await this.checkInsRespository.countByUserId(userId)
+
+    return { checkInsCount }
+  }
+}
