7944eaabf93aea6ceac98df3b5fe129245230327
Matheus <matheus.possenti24@gmail.com>
2024-10-18 15:16:42 -0300

    e2e register test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,24 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('Register (e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+    console.log(response.body)
+
+    expect(response.status).toBe(201)
+  })
+})
