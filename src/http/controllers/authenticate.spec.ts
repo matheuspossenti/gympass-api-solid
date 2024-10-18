b1dbea2b121e867d63b2d23b8f229b18b9b1f570
Matheus <matheus.possenti24@gmail.com>
2024-10-18 15:20:25 -0300

    e2e authenticate test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -2,7 +2,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
 
-describe('Register (e2e', () => {
+describe('Authenticate (e2e', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,13 +12,18 @@ describe('Register (e2e', () => {
   })
 
   it('should be able to register', async () => {
-    const response = await request(app.server).post('/users').send({
+    await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '12345678',
     })
-    console.log(response.body)
 
-    expect(response.statusCode).toBe(201)
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('token')
   })
 })
