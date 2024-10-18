0f8f12f8cfc158f341171a79f2aef0579aa8c840
Matheus <matheus.possenti24@gmail.com>
2024-10-18 11:50:24 -0300

    feat: jwt authorization and middleware

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,21 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const getProfileUseCase = makeGetUserProfileUseCase()
+
+    const { user } = await getProfileUseCase.execute({
+      userId: request.user.sub,
+    })
+
+    return reply.status(200).send({
+      user: {
+        ...user,
+        password_hash: undefined,
+      },
+    })
+  } catch (error) {}
+
+  return reply.status(200).send()
+}
